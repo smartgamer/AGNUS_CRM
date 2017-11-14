cca658abc501e38d91dbd121ab9e09c6fb059d65
Guimarães Mahota <guimaraesmahota@gmail.com>
2017-11-15 01:13:51 +0200

    Add Categories

--- client/accounts/NewAccount.js
+++ client/accounts/NewAccount.js
@@ -5,3 +5,9 @@ Template.NewAccount.onCreated(function(){
         self.subscribe('accounts');
     });
 });
+
+Template.NewAccount.events({
+    'click .fa-close': function(){
+        Session.set('NewAccount',false);
+    }
+});
